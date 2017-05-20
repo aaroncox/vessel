269bf5723ea6a87954ef4ce561048f4efabc3e6a
Aaron Cox <aaron.cox@greymass.com>
2017-05-20 14:20:19 -0400

    Added an escape route for the experimental section

--- app/components/Keys/Generate.js
+++ app/components/Keys/Generate.js
@@ -139,152 +139,16 @@ export default class KeysGenerate extends Component {
             <pre>{JSON.stringify(privatekeys, null, 2)}</pre>
           </Segment>
         </Segment>
+        <Segment basic textAlign="center">
+          <Button
+            size="large"
+            color="red"
+            content="Close"
+            onClick={this.props.handleMethodReset}
+          />
+        </Segment>
       </div>
     );
-
-
-
-    // const { account, wif, encryptWallet, lock1, lock2 } = this.state;
-    // const confirmAccount = this.props.keys.confirm;
-    // const errors = this.state.errors;
-    // const hasErrors = this.hasErrors();
-    // const messages = {
-    //   account_name_required: 'An existing account name is required.',
-    //   password_error_not_set: 'You must set a password to enable encryption.',
-    //   password_error_no_match: 'The passwords you have entered do not match.',
-    //   wif_error_invalid: 'They WIF Private Key you have entered is invalid.',
-    // };
-    // let passwordInput = false;
-    // let handleMethodReset = false;
-    // if (this.props.handleMethodReset) {
-    //   handleMethodReset = (
-    //     <Segment basic textAlign="center">
-    //       <Button
-    //         size="large"
-    //         color="red"
-    //         content="Cancel"
-    //         onClick={this.props.handleMethodReset}
-    //       />
-    //     </Segment>
-    //   );
-    // }
-    // let passwordWarning = (
-    //   <Message
-    //     icon="warning"
-    //     header="Notice about Unencrypted Wallets"
-    //     content="If you choose not to encrypt your wallet, your private keys will be stored on your computer unencrypted. This greatly increases the risk of your account being comprimised in the event your computer is hacked."
-    //   />
-    // );
-    // let message = false;
-    // let warning = false;
-    // if (hasErrors || this.props.keys.lastError) {
-    //   if (this.props.keys.lastError) {
-    //     message = this.props.keys.lastError;
-    //   }
-    //   if (errors.password_error_not_set) {
-    //     message = messages.password_error_not_set;
-    //   }
-    //   if (errors.password_error_no_match) {
-    //     message = messages.password_error_no_match;
-    //   }
-    //   if (errors.wif_error_invalid) {
-    //     message = messages.wif_error_invalid;
-    //   }
-    //   if (errors.account_name_required) {
-    //     message = messages.account_name_required;
-    //   }
-    //   warning = (
-    //     <Message
-    //       error
-    //       icon="warning"
-    //       content={message}
-    //     />
-    //   );
-    // }
-    // if (encryptWallet) {
-    //   passwordInput = (
-    //     <div>
-    //       <Form.Input label="Wallet Password" type="password" name="lock1" value={lock1} onChange={this.handleChange} />
-    //       <Form.Input label="Wallet Password (Repeat)" type="password" name="lock2" value={lock2} onChange={this.handleChange} />
-    //       <p>
-    //         The wallet password is only used to unlock your
-    //         accounts stored within the Vessel wallet.
-    //       </p>
-    //     </div>
-    //   );
-    //   passwordWarning = (
-    //     <Message
-    //       icon="warning"
-    //       header="Notice about Encryption"
-    //       content="If you lose your password, you will be unable to recover your keys from this application. Please make sure you have backed up your keys and remember your password."
-    //     />
-    //   );
-    // }
-    // let display = (
-    //   <div>
-    //     <Header>
-    //       <Header.Subheader>
-    //         Enter an account name and a WIF private key for the account
-    //         you would like to add. Different permissions will be granted
-    //         to this wallet based on the type of key used.
-    //       </Header.Subheader>
-    //     </Header>
-    //     <Divider hidden />
-    //     <Form
-    //       error={hasErrors || this.props.keys.lastError}
-    //       loading={this.props.processing.account_loading}
-    //     >
-    //       <Grid divided>
-    //         <Grid.Row>
-    //           <Grid.Column width={8}>
-    //             <Form.Input
-    //               label="Account Name"
-    //               name="account"
-    //               value={account}
-    //               onChange={this.handleChange}
-    //             />
-    //             <Form.Input
-    //               label="WIF Private Key (posting, active or owner)"
-    //               type="password"
-    //               name="wif"
-    //               value={wif}
-    //               onChange={this.handleChange}
-    //             />
-    //             <Divider hidden />
-    //             <Checkbox
-    //               toggle
-    //               name="encryptWallet"
-    //               label="Encrypt this Wallet for better security"
-    //               checked={encryptWallet}
-    //               value={encryptWallet ? 'off' : 'on'}
-    //               onChange={this.handleToggle}
-    //             />
-    //           </Grid.Column>
-    //           <Grid.Column width={8}>
-    //             {passwordInput}
-    //           </Grid.Column>
-    //         </Grid.Row>
-    //       </Grid>
-    //       <Divider hidden />
-    //       <Divider />
-    //       {passwordWarning}
-    //       {warning}
-    //       <Button disabled={hasErrors} fluid size="large" color="green" onClick={this.handleSubmit}>
-    //         Add to Wallet
-    //       </Button>
-    //       {handleMethodReset}
-    //     </Form>
-    //   </div>
-    // );
-    // if (confirmAccount) {
-    //   display = (
-    //     <KeysConfirm
-    //       handleConfirmAction={this.handleConfirmAction}
-    //       encryptWallet={this.state.encryptWallet}
-    //       {...this.props}
-    //     />
-    //   );
-    // }
     return display;
   }
 }
