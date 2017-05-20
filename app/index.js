cf99a44846d8729190f2e610baa593c53518cf19
Aaron Cox <aaron.cox@greymass.com>
2017-05-20 14:21:21 -0400

    Removed unused imports

--- app/index.js
+++ app/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
-import { persistStore, autoRehydrate } from 'redux-persist'
-import AppProvider from './app.js'
+import AppProvider from './app';
 
 render(
   <AppProvider />,
