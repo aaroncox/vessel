feb06024922d07aad1479ae8b4fa8db7b479ec1b
Augusto <augusto.lemble@gmail.com>
2017-05-22 23:27:26 -0300

    Added copy to clipboard on key generation view

--- app/components/Keys/Generate.js
+++ app/components/Keys/Generate.js
@@ -50,6 +50,57 @@ export default class KeysGenerate extends Component {
     this.setState({mnemonic, pub, prv});
   }
   render() {
+
+    function setClipboardText(text){
+      var id = "mycustom-clipboard-textarea-hidden-id";
+      var existsTextarea = document.getElementById(id);
+
+      if(!existsTextarea){
+        console.log("Creating textarea");
+        var textarea = document.createElement("textarea");
+        textarea.id = id;
+        // Place in top-left corner of screen regardless of scroll position.
+        textarea.style.position = 'fixed';
+        textarea.style.top = 0;
+        textarea.style.left = 0;
+
+        // Ensure it has a small width and height. Setting to 1px / 1em
+        // doesn't work as this gives a negative w/h on some browsers.
+        textarea.style.width = '1px';
+        textarea.style.height = '1px';
+
+        // We don't need padding, reducing the size if it does flash render.
+        textarea.style.padding = 0;
+
+        // Clean up any borders.
+        textarea.style.border = 'none';
+        textarea.style.outline = 'none';
+        textarea.style.boxShadow = 'none';
+
+        // Avoid flash of white box if rendered for any reason.
+        textarea.style.background = 'transparent';
+        document.querySelector("body").appendChild(textarea);
+        console.log("The textarea now exists :)");
+        existsTextarea = document.getElementById(id);
+      }else{
+          console.log("The textarea already exists :3")
+      }
+
+      existsTextarea.value = text;
+      existsTextarea.select();
+
+      try {
+        var status = document.execCommand('copy');
+        if(!status){
+          console.error("Cannot copy text");
+        }else{
+          console.log("The text is now on the clipboard");
+        }
+      } catch (err) {
+        console.log('Unable to copy.');
+      }
+    }
+
     let sharable = {};
     let privatekeys = {};
     console.log(this.state);
@@ -116,6 +167,12 @@ export default class KeysGenerate extends Component {
           <Segment>
             {this.state.mnemonic}
           </Segment>
+          <Button
+            color="green"
+            content="Copy to clipboard"
+            onClick={()=> setClipboardText(this.state.mnemonic)}
+            disabled={!this.state.mnemonic}
+          />
         </Segment>
         <Segment>
           <Header>
@@ -127,6 +184,12 @@ export default class KeysGenerate extends Component {
           <Segment>
             <pre>{JSON.stringify(sharable, null, 2)}</pre>
           </Segment>
+          <Button
+            color="green"
+            content="Copy to clipboard"
+            onClick={()=> setClipboardText(JSON.stringify(sharable, null, 2))}
+            disabled={!this.state.mnemonic}
+          />
         </Segment>
         <Segment>
           <Header>
@@ -138,6 +201,12 @@ export default class KeysGenerate extends Component {
           <Segment>
             <pre>{JSON.stringify(privatekeys, null, 2)}</pre>
           </Segment>
+          <Button
+            color="green"
+            content="Copy to clipboard"
+            onClick={()=> setClipboardText(JSON.stringify(privatekeys, null, 2))}
+            disabled={!this.state.mnemonic}
+          />
         </Segment>
         <Segment basic textAlign="center">
           <Button
