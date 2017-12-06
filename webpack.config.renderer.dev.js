dc0736cc32124bd654ef2c65897e9f2a8ee9f944
Aaron Cox <aaron.cox@greymass.com>
2017-12-05 20:07:44 -0500

    Changed setup() -> before()

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -237,7 +237,7 @@ export default merge.smart(baseConfig, {
       verbose: true,
       disableDotRule: false,
     },
-    setup() {
+    before() {
       if (process.env.START_HOT) {
         spawn(
           'npm',
